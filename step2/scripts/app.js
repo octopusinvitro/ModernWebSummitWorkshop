632e64e0ca6f4794a62ffdbd8de78e6d8eab6f67
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-23 13:46:12 +0100

    Step2

--- step2/scripts/app.js
+++ step2/scripts/app.js
@@ -97,3 +97,11 @@ app.selectedCityKeys.forEach((cityKey) => {
     app.updateForecastCard(cityKey, forecast);
   });
 });
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('./service-worker.js')
+  .then(function(){
+    console.log('Service Worker Registered');
+  });
+}
